8215da09fa6462129e6756dc09c37593c49d7571
YavuzSahin <yavuz.sahins@gmail.com>
2026-03-11 11:04:05 +0300

    first page

--- index.php
+++ index.php
@@ -253,92 +253,6 @@
             .article { padding: 24px 22px; }
         }
 
-        .faq {
-            text-align: center;
-            padding: 80px 24px;
-            border-top: 1px solid var(--border);
-        }
-
-        .faq-heading {
-            font-weight: 400;
-            font-size: 19px;
-            -webkit-transition: text-indent 0.2s;
-            text-indent: 20px;
-            color: #333;
-        }
-
-        .faq-text {
-            font-weight: 400;
-            color: #919191;
-            width:95%;
-            padding-left:20px;
-            margin-bottom:30px;
-        }
-
-        .faq {
-            width: 1000px;
-            margin: 0 auto;
-            background: white;
-            border-radius: 4px;
-            position: relative;
-            border: 1px solid #E1E1E1;
-        }
-        .faq label {
-            display: block;
-            position: relative;
-            overflow: hidden;
-            cursor: pointer;
-            height: 56px;
-            padding-top:1px;
-
-            background-color: #FAFAFA;
-            border-bottom: 1px solid #E1E1E1;
-        }
-
-        .faq input[type="checkbox"] {
-            display: none;
-        }
-
-        .faq .faq-arrow {
-            width: 5px;
-            height: 5px;
-            transition: transform 0.8s, -webkit-transform 0.8s;
-            -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
-            border-top: 2px solid rgba(0, 0, 0, 0.33);
-            border-right: 2px solid rgba(0, 0, 0, 0.33);
-            float: right;
-            position: relative;
-            top: -30px;
-            right: 27px;
-            -webkit-transform: rotate(45deg);
-            transform: rotate(45deg);
-        }
-
-        .faq input[type="checkbox"]:checked + label > .faq-arrow {
-            transition: transform 0.8s, -webkit-transform 0.8s;
-            -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
-            -webkit-transform: rotate(135deg);
-            transform: rotate(135deg);
-        }
-        .faq input[type="checkbox"]:checked + label {
-            display: block;
-            background: rgba(255,255,255,255) !important;
-            color: #4f7351;
-            height: 225px;
-            transition: height 0.8s;
-            -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
-        }
-
-        .faq input[type='checkbox']:not(:checked) + label {
-            display: block;
-            transition: height 0.8s;
-            height: 60px;
-            -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
-        }
-
-        ::-webkit-scrollbar {
-            display: none;
-        }
     </style>
 
 
@@ -687,14 +601,13 @@
 
     </div>
 </section>
-<section id="faq" class="faq">
-    <div class='faq'>
-        <input id='faq-a' type='checkbox'>
-        <label for='faq-a'>
-            <p class="faq-heading">How do I purchase Brainitz?</p>
-            <div class='faq-arrow'></div>
-            <p class="faq-text">You can purchase an individual account or a site license by emailing us at welcome@brainitz.com One of our staff will be in touch shortly to set-up your account. We accept credit card, purchase order, or check. Learn more on our pricing page.<br><br>We also offer professional development trainings that include the opportunity to pursue credit while also receiving a discounted license. If you have more questions, please feel free to submit a request!</p>
-        </label>
+<section id="faq" class="section">
+    <div class="tab">
+        <input type="checkbox" name="accordion-1" id="cb1" checked>
+        <label for="cb1" class="tab__label">Checkbox</label>
+        <div class="tab__content">
+            <p>Pure CSS accordion based on the "input:checked + label" style trick.</p>
+        </div>
     </div>
 </section>
 
