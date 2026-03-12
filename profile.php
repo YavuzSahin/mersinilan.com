ad8db6add60ef036ed814ef100657be6c1018d51
YavuzSahin <yavuz.sahins@gmail.com>
2026-03-12 12:22:49 +0300

    first page

--- profile.php
+++ profile.php
@@ -809,11 +809,62 @@ $arrayVar = [
 <section class="section" id="<?=$id;?>">
     <a href="<?=$site;?>/profil/<?=$id;?>" title="<?=$details['title'];?>" style="color: var(--red);"><h2><?=$details['title'];?></h2></a>
     <p class="section-desc" style="text-align: left;"><?=$details['profile_text'];?></p>
+
+    <div class="gallery">
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485766410122-1b403edb53db?dpr=1&auto=format&fit=crop&w=1500&h=846&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485793997698-baba81bf21ab?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485871800663-71856dc09ec4?dpr=1&auto=format&fit=crop&w=1500&h=2250&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485871882310-4ecdab8a6f94?dpr=1&auto=format&fit=crop&w=1500&h=2250&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485872304698-0537e003288d?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485872325464-50f17b82075a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1470171119584-533105644520?dpr=1&auto=format&fit=crop&w=1500&h=886&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485881787686-9314a2bc8f9b?dpr=1&auto=format&fit=crop&w=1500&h=969&q=80&cs=tinysrgb&crop=" alt="" /></div>
+        <div class="img-w"><img src="https://images.unsplash.com/photo-1485889397316-8393dd065127?dpr=1&auto=format&fit=crop&w=1500&h=843&q=80&cs=tinysrgb&crop=" alt="" /></div>
+    </div>
 </section>
 
 
 <section class="section" id="mersin-escort">
     <p class="section-desc" style="text-align: left;"><?=$seoText;?></p>
 </section>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+    $(function() {
+        $(".img-w").each(function() {
+            $(this).wrap("<div class='img-c'></div>")
+            let imgSrc = $(this).find("img").attr("src");
+            $(this).css('background-image', 'url(' + imgSrc + ')');
+        });
+
+
+        $(".img-c").click(function() {
+            let w = $(this).outerWidth()
+            let h = $(this).outerHeight()
+            let x = $(this).offset().left
+            let y = $(this).offset().top
+
+
+            $(".active").not($(this)).remove()
+            let copy = $(this).clone();
+            copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+            $(".active").css('top', y - 8);
+            $(".active").css('left', x - 8);
+
+            setTimeout(function() {
+                copy.addClass("positioned")
+            }, 0)
+        });
+    })
+
+    $(document).on("click", ".img-c.active", function() {
+        let copy = $(this)
+        copy.removeClass("positioned active").addClass("postactive")
+        setTimeout(function() {
+            copy.remove();
+        }, 500);
+    });
+</script>
 </body>
 </html>
